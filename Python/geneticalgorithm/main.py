798944194ad434ce9dfdac678d50cd8ada7fe050
li195111 <green07111@gmail.com>
2022-11-29 14:47:20 +0800

    update

--- Python/geneticalgorithm/main.py
+++ Python/geneticalgorithm/main.py
@@ -19,3 +19,82 @@
   突變也會依據突變機率來改變
   為了避免最後的結果陷入區域最佳解的狀況
 '''
+from typing import List
+import numpy as np
+
+
+class Gene:
+  NAME = 'GENE'
+  def __init__(self, color:str, color_display:bool, display:bool) -> None:
+    self.__color = color
+    self.__color_display = color_display
+    self.__display = display
+  
+  def __repr__(self) -> str:
+    if self.color_display:
+      return f'{self.color} {self.NAME}'
+    return f'{self.NAME}'
+  
+  @property
+  def color(self):
+    return self.__color
+  
+  @property
+  def color_display(self):
+    return self.__color_display
+  
+  @property
+  def display(self):
+    return self.__display
+
+class Hair(Gene):
+  NAME = 'Hair'
+  def __init__(self, color: str, color_display: bool, display: bool) -> None:
+    super().__init__(color, color_display, display)
+
+class Eyes(Gene):
+  NAME = 'Eyes'
+  def __init__(self, color: str, color_display: bool, display: bool) -> None:
+    super().__init__(color, color_display, display)
+
+
+colors = ['red','green','blue','brown','black','white','none']
+color_display = [True, False]
+
+hair_gene_set = [Hair(color, color_display=True, display=True) for color in colors]
+eyes_gene_set = [Eyes(color, color_display=True, display=True) for color in colors]
+gene_set = hair_gene_set + eyes_gene_set
+print(gene_set)
+# init_size = 50
+# init_genes = np.concatenate([np.random.choice(colors, (init_size,1)),
+#                              np.random.choice(gene_set, (init_size,1))],1)
+# init_genes = np.array(list(map(lambda x: ' '.join(x), init_genes)))
+# init_gene_set = set(init_genes)
+# init_gene_set_size = len(init_gene_set)
+# print(init_gene_set)
+
+# chro_size = len(gene_set)
+# popu_size = 23
+
+
+class Chromosome:
+  def __init__(self, size:int) -> None:
+    self.size = size
+    self.genetics:List[Gene] = []
+  
+  def add(self, gene:Gene):
+    if len(self.genetics) < self.size:
+      self.genetics.append(gene)
+  
+  def fitness_value(self, func):
+    return func(self)
+
+class Population:
+  def __init__(self, size:int) -> None:
+    self.size = size
+    self.chromosomes:List[Chromosome] = []
+  
+  def add(self, chromosome:Chromosome):
+    if len(self.chromosomes) < self.size:
+      self.chromosomes.append(chromosome)
+  
